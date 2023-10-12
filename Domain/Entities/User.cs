8f3b126849d41be7ba665e5595ce892a2556131d
IgmarLozadaBolivar <losadabolivar@gmail.com>
2023-10-12 08:00:53 -0500

    feat: :sparkles: User entity defined
    Entidad User definida con sus propiedades

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,8 @@
+namespace Domain.Entities;
+
+public class User : BaseEntity
+{
+    public string Email { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
