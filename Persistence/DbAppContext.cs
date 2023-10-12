7d6fccfa823a3c1a47fa12b83cdd0d54724eb54c
IgmarLozadaBolivar <losadabolivar@gmail.com>
2023-10-12 08:02:50 -0500

    feat: :sparkles: The context of the User Entity was implemented
    Se implemento el contexto de la entidad User

--- Persistence/DbAppContext.cs
+++ Persistence/DbAppContext.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 namespace Persistence;
 
@@ -7,6 +8,8 @@ public class DbAppContext : DbContext
     public DbAppContext(DbContextOptions<DbAppContext> options) : base(options)
     {}
     
+    public DbSet<User> Users { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
