f98528542999dd4a58b7dd161a3b7446febafc6a
IgmarLozadaBolivar <losadabolivar@gmail.com>
2023-10-13 07:20:06 -0500

    feat: :sparkles: Migrations enabled
    Migraciones habilitadas, se creo la primera migracion, se considera mas migraciones a futuro

---
+++ Persistence/Data/Migrations/20231012131030_Initial.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Persistence.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", maxLength: 3, nullable: false, comment: "ID del usuario")
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Email = table.Column<string>(type: "varchar(150)", maxLength: 150, nullable: false, comment: "Correo del usuario"),
+                    Username = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false, comment: "Nombre del usuario"),
+                    Password = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false, comment: "Contraseña del usuario")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
